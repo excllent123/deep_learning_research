c12cd2937ee22ae6914fe7bf3f58f7cacffb2fdf
KentChun33333 <kentchun33333@gmail.com>
2016-09-07 17:35:00 -0400

    -m save_load pair with weight

---
+++ __init__.py
@@ -0,0 +1,31 @@
+import ipywidgets import interact, interactive, fixed
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+from sklearn.cross_validation import KFold
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Convolution2D, MaxPooling2D
+from keras.utils import np_utils
+from keras.optimizers import SGD
+from keras.optimizers import Adam
+from keras.utils import np_utils
+from keras.models import model_from_json
+
+
+import numpy as np
+np.random.seed(1337)  # for reproducibility
+import os, sys, progressbar, argparse
+import commentjson as json
+
+from imutils import paths
+from skimage.io import imread
+import imageio
+import cv2
