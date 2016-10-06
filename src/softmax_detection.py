f47f480ea1947bff45ccae86d94ee773e0bbb389
KentChun33333 <kentchun33333@foxmail.com>
2016-10-06 15:49:17 -0400

    -m

--- src/softmax_detection.py
+++ src/softmax_detection.py
@@ -1,103 +1,92 @@
 # import the necessary packages
 import common_func
 
-class ObjectDetector:
-    def __init__(self, model, desc):
-        # store the classifier and HOG descriptor
-        self.model = model
-        self.desc = desc
-
-    def detect(self, image, winDim, winStep=4, pyramidScale=1.5, minProb=0.7):
-        # initialize the list of bounding boxes and associated probabilities
-        memo = {}
-        boxes = []
-        probs = []
-
-        # loop over the image pyramid
-        for layer in common_func.pyramid(image, scale=pyramidScale, minSize=winDim):
-            # determine the current scale of the pyramid
-            scale = image.shape[0] / float(layer.shape[0])
-
-            # loop over the sliding windows for the current pyramid layer
-            for (x, y, window) in common_func.sliding_window(layer, winStep, winDim):
-
-                # grab the dimensions of the window
-                (winH, winW) = window.shape[:2]
-
-                # ensure the window dimensions match the supplied sliding window dimensions
-                if winH == winDim[1] and winW == winDim[0]:
-                    # extract HOG features from the current window and classifiy whether or
-                    # not this window contains an object we are interested in
-                    features = self.desc.describe(window)
-                    features.reshape(-1, 1) # due to the depreciation
-                    probS = self.model.predict_proba(features)[0][1]
-
-                    ##################
-                    # softmax return multiple probability value
-                    for label, prob in enumerate(probS):
-                        if prob>minProb:
-                            (startX, startY) = (int(scale * x), int(scale * y))
-                            endX = int(startX+(scale * winW))
-                            endY = int(startY+(scale * winH))
+import gc
+# http://stackoverflow.com/questions/22440421/python-is-the-garbage-collector-run-before-a-memoryerror-is-raised
+# if there are hidden reference cycles
+# use gc.collect() to manually release the memory in each iteration
 
 
-                    # check to see if the classifier has found an object with sufficient
-                    # probability
-                    if prob > minProb:
-                        # compute the (x, y)-coordinates of the bounding box using the current
-                        # scale of the image pyramid
+def singleton(class_):
+  instances = {}
+  def getinstance(*args, **kwargs):
+    if class_ not in instances:
+        instances[class_] = class_(*args, **kwargs)
+    return instances[class_]
+  return getinstance
+
+@singleton
+class Memo(object):
+    def __init__(self, numLabel):
+        self.boxes={}
+        self.prob ={}
+        for i in range(numLabel):
+            self.boxes[i]=[]
+            self.prob[i]=[]
+
+    def addNode(self, labelID, box, prob):
+        assert type(box)==tuple
+        self.boxes[labelID].append(box)
+        self.prob[labelID].append(prob)
+
+    def extratResult(self,labelID):
+        try :
+            return self.boxes[labelID], self.prob[labelID]
+        except Exception as err:
+            print (err)
+
+# windim = (x,y) = 32, 32 this case
+def detection_test(x, model, winDim, pyramidScale=20,winStep=20, minProb=0.9995, numLabel=2):
+    '''
+    numLable = y-label number
+    in this case, we have 2 lebels 0 and 1
+    '''
+    assert type(x)==int
+    assert x>=0
+    image = vid.get_data(x)
+    image = imutils.resize(image, width=200)
+
+    memo = Memo(numLabel)
+
+    orig = image.copy()
+    img_gray = cv2.cvtColor( image , cv2.COLOR_BGR2GRAY)
+    # loop over the image pyramid
+    for layer in common_func.pyramid(img_gray, scale=pyramidScale, minSize=winDim):
+        # determine the current scale of the pyramid
+        scale = image.shape[0] / float(layer.shape[0])
+
+        # loop over the sliding windows for the current pyramid layer
+        for (x, y, window) in common_func.sliding_window(layer, winStep, winDim):
+            # grab the dimensions of the window
+            (winH, winW) = window.shape[:2]
+
+
+            # ensure the window dimensions match the supplied sliding window dimensions
+            if winH == winDim[1] and winW == winDim[0]:
+                window = window.reshape(1,1,winW,winH)
+
+                #return window [0][0]
+                probS = model.predict_proba(window,verbose=0)[0]
+
+                # check to see if the classifier has found an object with sufficient
+                # probability
+
+                for label, prob in enumerate(probS):
+                    if prob>minProb:
                         (startX, startY) = (int(scale * x), int(scale * y))
-                        endX = int(startX + (scale * winW))
-                        endY = int(startY + (scale * winH))
-
-                        # update the list of bounding boxes and probabilities
-                        boxes.append((startX, startY, endX, endY))
-                        probs.append(prob)
-
-        # return a tuple of the bounding boxes and probabilities
-        return (boxes, probs)
-
-
-class MultiObjectDetection(ObjectDetector):
-    def __init__(self,labNums,model, desc):
-        ObjectDetector.__init__(self, model, desc)
-        self.labNums = labNums
-
-    def detect(self, image, winDim, winStep=4, pyramidScale=1.5, minProb=0.7):
-        # initialize the list of bounding boxes and associated probabilities
-        labels = []
-        boxes = []
-        probs = []
-
-        # loop over the image pyramid
-        for layer in common_func.pyramid(image, scale=pyramidScale, minSize=winDim):
-            # determine the current scale of the pyramid
-            scale = image.shape[0] / float(layer.shape[0])
-
-            # loop over the sliding windows for the current pyramid layer
-            for (x, y, window) in common_func.sliding_window(layer, winStep, winDim):
-
-                # grab the dimensions of the window
-                (winH, winW) = window.shape[:2]
-
-                # ensure the window dimensions match the supplied sliding window dimensions
-                if winH == winDim[1] and winW == winDim[0]:
-                    # extract HOG features from the current window and classifiy whether or
-                    # not this window contains an object we are interested in
-                    features = self.desc.describe(window)
-                    features.reshape(-1, 1) # due to the depreciation
-                    probS = self.model.predict_proba(features)[0][1]
-
-                    ##################
-                    # softmax return multiple probability value
-                    for label, prob in enumerate(probS):
-                        if prob>minProb:
-                            (startX, startY) = (int(scale * x), int(scale * y))
-                            endX = int(startX+(scale * winW))
-                            endY = int(startY+(scale * winH))
-                            #
-                            labels.append(label)
-                            boxes.append((startX, startY, endX, endY))
-                            probs.append(prob)
-
-        return np.array([labels, boxes, probs])
+                        endX = int(startX+(scale * winW))
+                        endY = int(startY+(scale * winH))
+                        #
+                        box = (startX, startY, endX, endY)
+                        memo.addNode(label, box, prob)
+    print ('END Detect')
+    for label in range(numLabel):
+        boxes, probs = memo.extratResult(label)
+        pick = common_func.non_max_suppression(np.array(boxes), probs, 0.7)
+        # loop over the allowed bounding boxes and draw them
+        for (startX, startY, endX, endY) in pick:
+            if startY < 10 or startX < 10 or abs(startX - endX)>50 or abs(startX - endX)<15 :
+                pass
+            else:
+                cv2.rectangle(orig, (startX, startY), (endX, endY), (0, 255, label*255), 2)
+    return orig
