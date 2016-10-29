4cc265ad2cb3518876064512c1bf5317a54ad17e
KentChun33333 <kentchun33333@gmail.com>
2016-10-29 07:45:00 -0400

    -m

---
+++ model_script/YOLO.py
@@ -0,0 +1,8 @@
+
+
+# Structure 
+# - Encoder : Image(Resolution) + Ann -> Ground truth Y
+# set S, B, C ... in the encoder.py
+# - Decoder : Predict Y -> Ann | Image(Resolution)
+# - Customized Loss Function 
+# - 
