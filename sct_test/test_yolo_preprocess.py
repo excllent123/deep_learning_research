20e1d96f533915124e4103fc5238458f8657d845
KentChun33333 <kentchun33333@gmail.com>
2016-11-09 17:58:56 -0500

    -m

--- sct_test/test_yolo_preprocess.py
+++ sct_test/test_yolo_preprocess.py
@@ -1,7 +1,30 @@
+
 from ..YOLO.yolo_preprocess import YoloPreprocess
 
-from unittest import TestCase
-import pytest
+import unittest
 
 
-class yolo_df
+
+class TestPipeLine(unittest.TestCase):
+    filepath = '../data_test/vtic_example.txt'
+    maplist = ['Rhand', 'ScrewDriver']
+    yolo_processor = YoloPreprocess(filepath, maplist=maplist)
+    
+
+    def test_df(self):
+
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+if __name__ == '__main__':
+    unittest.main()
