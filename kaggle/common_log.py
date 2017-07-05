8183c37822aadb08ab99c216c5c7d77017739b65
KentChun33333 <kentchun33333@gmail.com>
2017-07-05 23:25:09 +0800

    kaggle deep regressor

--- kaggle/common_log.py
+++ kaggle/common_log.py
@@ -1,40 +1,40 @@
 import logging 
 
 class DebugLog(object):
-	def __init__(self, name):
-		#super(DebugLog, self)__init__()
-		self.log = logging.getLogger(name)
-		self.format = logging.Formatter('%(asctime)s, %(name)s'
-			                            ',%(levelname)s, %(message)s')
-		self.add_stream_log()
-		#self.add_file_log()
+    def __init__(self, name):
+        #super(DebugLog, self)__init__()
+        self.log = logging.getLogger(name)
+        self.format = logging.Formatter('%(asctime)s, %(name)s'
+                                        ',%(levelname)s, %(message)s')
+        self.add_stream_log()
+        #self.add_file_log()
 
-	def add_stream_log(self):
-		# if not hasattr('ch'):
-		self.ch = logging.StreamHandler()
-		# [Note] setLevel = ignore level
-		self.ch.setLevel(logging.DEBUG)
-		self.ch.setFormatter(self.format)
-		self.log.addHandler(self.ch)
+    def add_stream_log(self):
+        # if not hasattr('ch'):
+        self.ch = logging.StreamHandler()
+        # [Note] setLevel = ignore level
+        self.ch.setLevel(logging.DEBUG)
+        self.ch.setFormatter(self.format)
+        self.log.addHandler(self.ch)
 
-	def add_file_log(self, name):
-		self.fh = logging.FileHandler(name)
-		self.fh.setLevel(logging.DEBUG)
-		self.fh.setFormatter(self.format)
-		self.log.addHandler(self.fh)
+    def add_file_log(self, name):
+        self.fh = logging.FileHandler(name)
+        self.fh.setLevel(logging.DEBUG)
+        self.fh.setFormatter(self.format)
+        self.log.addHandler(self.fh)
 
-	def warn(self,txt):
-		return self.log.warn(txt)
+    def warn(self,txt):
+        return self.log.warn(txt)
 
-	def info(self, txt):
-		return self.log.info(txt)
+    def info(self, txt):
+        return self.log.info(txt)
 
-	def critic(self, txt):
-		return self.log.critic(txt)
+    def critic(self, txt):
+        return self.log.critic(txt)
 
 if __name__ =='__main__':
-	test = DebugLog('s')
-	test.warn('OK')
+    test = DebugLog('s')
+    test.warn('OK')
 
 # =============================================================== #
 # [Note]                                                          #
