540a55475be53d0fb2141564577d867078dfd7f3
Kent Chiu <kentchun33333@foxmail.com>
2017-08-23 13:43:59 +0800

    up

--- china_tax__tw_.py
+++ china_tax__tw_.py
@@ -1,6 +1,6 @@
 
 
-tax = [(   4800  , 0    ),
+tax_china = [(   4800  , 0    ),
        (   1500  , 0.03 ),
        (   3000  , 0.1  ), 
        (   4500  , 0.2  ),
@@ -11,7 +11,7 @@ tax = [(   4800  , 0    ),
 
 
 
-def tax_china(x, tax_rule):
+def count_tax(x, tax_rule):
     res=0
     for i, j in tax:
         if x-i>0:
