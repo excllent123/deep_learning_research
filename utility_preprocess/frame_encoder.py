00810d1497a4bd156b9d3c3378034a3843c89d7f
Kent Chiu <kentchun33333@foxmail.com>
2018-08-19 01:10:26 +0800

    upframeencoder

--- utility_preprocess/frame_encoder.py
+++ utility_preprocess/frame_encoder.py
@@ -1,8 +1,10 @@
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder, MinMaxScaler, StandardScaler
 
 class PandasBaseAgent():
-    def __init__(self):
-        pass
+    def __init__(self, except_cols=[]):
+        if type(except_cols)== str:
+            except_cols = [except_cols]
+        self.except_cols= except_cols
 
     def fit(self, df):
         '''
@@ -14,61 +16,68 @@ class PandasBaseAgent():
         if not self.cols: raise('need fit first')
         r_df = df.copy()
         for col in self.cols: 
-            r_df[col] = self.le[col].transform(r_df[col])
+            try:
+                r_df[col] = self.le[col].transform(r_df[col].values.reshape(-1, 1))
+            except Exception as e : 
+                print(col, ' : ' ,e)
         return r_df 
 
     def inverse_transform(self, df):
         if not self.cols: raise('need fit first')
         r_df = df.copy()
         for col in self.cols: 
-            r_df[col] = self.le[col].inverse_transform(r_df[col])
+            r_df[col] = self.le[col].inverse_transform(r_df[col].values.reshape(-1, 1))
         return r_df
 
 class PandasLabelEncoder(PandasBaseAgent):
-    def __init__(self):
-        super(PandasLabelEncoder, self).__init__()
+    def __init__(self, *arg, **kwargs):
+        super(PandasLabelEncoder, self).__init__(*arg, **kwargs)
     
     def fit(self, df):
-        self.cols =  [i for i in df.columns if df.dtypes[i]=='object']
+        self.cols =  [i for i in df.columns if df.dtypes[i]=='object' ]
+        self.cols = [i for i in self.cols if i not in self.except_cols]
         self.le = {}
         for col in self.cols:
             self.le[col] = LabelEncoder()
-            self.le[col].fit(list(df[col]))
+            self.le[col].fit(df[col].values.reshape(-1, 1))
 
 
 
 class PandasOneHotEncoder(PandasBaseAgent):
-    def __init__(self):
-        super(PandasOneHotEncoder, self).__init__()
+    def __init__(self, *arg, **kwargs):
+        super(PandasOneHotEncoder, self).__init__(*arg, **kwargs)
         
     def fit(self, df):
         self.cols =  [i for i in df.columns if df.dtypes[i] in ['int8', 'int32', 'int64'] ]
+        self.cols = [i for i in self.cols if i not in self.except_cols]
         self.le = {}
         for col in self.cols:
             self.le[col] = OneHotEncoder()
-            self.le[col].fit(list(df[col]))
+            self.le[col].fit(df[col].values.reshape(-1, 1))
 
 class PandasMinMaxScaler(PandasBaseAgent):
-    def __init__(self):
-        super(PandasMinMaxScaler, self).__init__()
+    def __init__(self, *arg, **kwargs):
+        super(PandasMinMaxScaler, self).__init__(*arg, **kwargs)
         
     def fit(self, df):
         self.cols =  [i for i in df.columns if df.dtypes[i] in ['int8', 
                             'int32', 'int64', 'float64', 'float32'] ]
+        self.cols = [i for i in self.cols if i not in self.except_cols]
         self.le = {}
         for col in self.cols:
             self.le[col] = MinMaxScaler()
-            self.le[col].fit(df[col].values.flatten())
+            self.le[col].fit(df[col].values.reshape(-1, 1))
 
 
 class PandasStandardScaler(PandasBaseAgent):
-    def __init__(self):
-        super(PandasStandardScaler, self).__init__()
+    def __init__(self, *arg, **kwargs):
+        super(PandasStandardScaler, self).__init__(*arg, **kwargs)
         
     def fit(self, df):
         self.cols =  [i for i in df.columns if df.dtypes[i] in ['int8', 
                             'int32', 'int64', 'float64', 'float32'] ]
+        self.cols = [i for i in self.cols if i not in self.except_cols]
         self.le = {}
         for col in self.cols:
             self.le[col] = StandardScaler()
-            self.le[col].fit(df[col].values.flatten())
+            self.le[col].fit(df[col].values.reshape(-1, 1))
