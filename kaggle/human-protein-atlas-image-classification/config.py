b482a021cc6a594b374c0cd8ace20a1061f20b4b
Kent Chiu <kentchun33333@foxmail.com>
2018-12-18 00:25:12 +0800

    kaggle-human-bsl setting

--- kaggle/human-protein-atlas-image-classification/config.py
+++ kaggle/human-protein-atlas-image-classification/config.py
@@ -1,10 +1,12 @@
+from hub_model.bsl_model import get_net
 class DefaultConfigs(object):
     train_data = "C:/Users/kent/.kaggle/competitions/human-protein-atlas-image-classification/train/" # where is your train data
     test_data = "C:/Users/kent/.kaggle/competitions/human-protein-atlas-image-classification/test/"   # your test data
     weights = "hub_ckpts/"
     best_models = "hub_ckpts/best_models/"
-    submit = "./submit/"
+    submit = "hub_submit/"
     model_name = "bninception_bcelog"
+    model = get_net()
     num_classes = 28
     img_weight = 512
     img_height = 512
